b29a2cdf80d6b3a4c0079a56e4d991ddb0d7a727
Sachin Mishra <msachin786.pm@gmail.com>
2022-09-30 23:57:55 +0530

    signup page added

---
+++ JS/login.js
@@ -0,0 +1,25 @@
+function loginFun() {
+    // setTimeout(() => {
+    //   window.location.href = "otp.html";
+    // }, 1000);
+    // let mobileNumber = document.querySelector("#mobileNumber").value;
+    alert("its working")
+}
+
+
+function inputFun() {
+  // here mobileNumber is the ID of input tag and login_button is the ID of login button
+  if (loginInput.value.length > 0) {
+    login_button.style.cursor = "pointer";
+    login_button.style.backgroundColor = "rgb(128, 53, 194)";
+    login_button.style.border = "none";
+    login_button.style.color = "white";
+    login_button.removeAttribute("disabled");
+  } else {
+    login_button.setAttribute("disabled", "disabled");
+    login_button.style.cursor = "not-allowed";
+    login_button.style.backgroundColor = "transparent";
+    login_button.style.color = "#cfcdcb";
+    login_button.style.border = "1px solid #cfcdcb";
+    }
+}
